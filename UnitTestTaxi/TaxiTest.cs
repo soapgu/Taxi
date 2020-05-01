ca35f5e41b607204915719fed4e770bab2827f6e
guhui <guhui@shgbit.com>
2020-05-01 22:27:36 +0800

    commit

---
+++ UnitTestTaxi/TaxiTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+
+namespace UnitTestTaxi
+{
+    [TestClass]
+    public class TaxiTest
+    {
+        
+        /// <summary>
+        /// 测试数据
+        /// </summary>
+        public string[] strTestDatas = null;
+        /// <summary>
+        /// 期望值
+        /// </summary>
+        public string[] strReceiptDatas = new string[4];
+
+        #region 测试函数
+        /// <summary>
+        /// 测试函数 
+        /// </summary>
+        [TestMethod]
+        public void Test()
+        {
+            
+            //读取测试数据
+            LoadTestData();
+
+            //公里数
+            int kilometre;
+            //等候时间
+            int waitingtime;
+
+            //车费计算类
+            fare testfare = new fare();
+
+            //顺序对测试文件进行测试
+            for (int i =0; i<= strTestDatas.Length-1;i++)
+            {
+                //公里数
+                kilometre = Convert.ToInt16(strTestDatas[i].Split(',')[0].Replace("公里", "").ToString());
+                //等候时间
+                waitingtime = Convert.ToInt16(strTestDatas[i].Split(',')[1].Replace("等待", "").Replace("分钟", "").ToString());
+                //返回值
+                string ret = "";
+
+                //计算
+                ret = testfare.Calculatefare(kilometre, waitingtime);
+
+                //对比测试结果
+                Assert.AreEqual(strReceiptDatas[i].ToString(), ret);
+            }
+
+        }
+
+        #endregion
+
+        #region 读取测试数据
+        /// <summary>
+        /// 读取测试数据
+        /// </summary>
+        /// <returns></returns>
+        private void LoadTestData()
+        {
+
+            strTestDatas = File.ReadAllLines("testData.txt",Encoding.GetEncoding("GB2312"));
+
+            strReceiptDatas[0] = "收费6元";
+            strReceiptDatas[1] = "收费7元";
+            strReceiptDatas[2] = "收费13元";
+            strReceiptDatas[3] = "收费7元";     
+
+        }
+
+        #endregion
+     
+    }
+}
---
+++ UnitTestTaxi/fare.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+//出租车计价：
+//不大于2公里时只收起步价6元。
+//超过2公里的部分每公里收取0.8元。
+//超过8公里的部分，每公里加收50%长途费。
+//停车等待时加收每分钟0.25元。
+//最后计价的时候司机会四舍五入只收到元。
+
+namespace UnitTestTaxi
+{
+    /// <summary>
+    /// 测试类
+    /// </summary>
+    class fare
+    {
+
+        #region 根据测试数据计费
+        /// <summary>
+        /// 计算车费
+        /// </summary>
+        /// <param name="kilometre">公里</param>
+        /// <param name="waitingtime">时间（分钟）</param>
+        /// <returns></returns>
+        public string Calculatefare(int kilometre, int waitingtime)
+        {
+
+            //最终车费
+            double ifare = 0;
+
+            //以公里数计算车费
+            ifare = Calculate_Kilometre_fare(kilometre);
+            //以等候时间计算车费 + 公里数车费
+            ifare = ifare + Calculate_waiting_fare(waitingtime);
+
+            return "收费" + Math.Round( ifare).ToString() + "元";
+
+        }
+        #endregion
+
+        #region 以公里数计算车费
+        /// <summary>
+        /// 以公里数计算车费
+        /// </summary>
+        /// <param name="kilometre">公里数</param>
+        /// <returns></returns>
+        private double Calculate_Kilometre_fare(int kilometre)
+        {
+            if (kilometre <=2)
+            {
+                return 6;
+            }
+            else if (kilometre > 2 && kilometre <= 8)
+            {
+                return Convert.ToDouble(((kilometre - 2) * 0.8) + 6);
+            }
+            else if (kilometre > 8)
+            {
+                return Convert.ToDouble((6 * 0.8) + ((kilometre - 8) * 1.2)  + 6);
+            }
+            return 0;
+        }
+        #endregion
+
+        #region 以等候时间计算车费
+        /// <summary>
+        /// 以等候时间计算车费
+        /// </summary>
+        /// <param name="waiting">等候时间</param>
+        /// <returns></returns>
+        private double Calculate_waiting_fare(int waiting)
+        {
+            return Convert.ToDouble(waiting * 0.25);
+        }
+        #endregion
+
+
+    }
+}
